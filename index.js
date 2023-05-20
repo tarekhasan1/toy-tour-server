de19324e33c0bab1279f2db42370f767c087b227
Md. Tarek Hasan <tarekhasan.tkg101@gmail.com>
2023-05-20 22:23:28 +0600

    successfully get all data from database

--- index.js
+++ index.js
@@ -25,6 +25,18 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+
+    const carsCollection = client.db('toyTour').collection('toycars');
+
+    app.get('/cars', async(req, res) =>{
+        const cursor = carsCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
