dc32c10275d29e4f341b7bbb213095fdd4b320ca
Md. Tarek Hasan <tarekhasan.tkg101@gmail.com>
2023-05-21 19:31:09 +0600

    removed await before client connection

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    client.connect();
 
 
     const carsCollection = client.db('toyTour').collection('toycars');
